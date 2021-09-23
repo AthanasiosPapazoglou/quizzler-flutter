47ae232d0ba88f1fe01d4d3ddb507d1a4b4f008b
Athanasios Papapzoglou <athanasiospap95@gmail.com>
2021-09-23 21:09:03 +0300

    custom class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -26,16 +27,19 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  
   List<Widget> scoreList = [];
 
-  List<String> questionsDB = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
+  List<Question> questionsDB = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(q: 'Approximately one quarter of human bones are in the feet.', a: true),
+    Question(q: 'A slug\'s blood is green.', a: true),
   ];
+
   int currentQ = 0;
 
   List<bool> answers = [false,true,true];
+
 
   @override
   Widget build(BuildContext context) {
@@ -49,7 +53,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionsDB[currentQ],
+                questionsDB[currentQ].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -73,7 +77,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswer = answers[currentQ];
+                bool correctAnswer = questionsDB[currentQ].questionAnswer;
 
                 if(correctAnswer == true){
                   print('You are RIGHT!');
@@ -101,7 +105,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswer = answers[currentQ];
+                bool correctAnswer = questionsDB[currentQ].questionAnswer;
 
                 if(correctAnswer == false){
                   print('You are RIGHT!');
