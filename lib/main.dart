f17d2db47eedadc54caf856df3a67baf8a00e602
Athanasios Papapzoglou <athanasiospap95@gmail.com>
2021-09-23 17:42:37 +0300

    initialise

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.grey.shade900,
         body: SafeArea(
@@ -25,6 +26,17 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Widget> scoreList = [];
+
+  List<String> questionsDB = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+  int currentQ = 0;
+
+  List<bool> answers = [false,true,true];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +49,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questionsDB[currentQ],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +73,17 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                bool correctAnswer = answers[currentQ];
+
+                if(correctAnswer == true){
+                  print('You are RIGHT!');
+                }else{
+                  print('You are WRONG!');
+                }
+
+                setState(() {
+                  currentQ++;
+                });
               },
             ),
           ),
@@ -79,12 +101,23 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                bool correctAnswer = answers[currentQ];
+
+                if(correctAnswer == false){
+                  print('You are RIGHT!');
+                }else{
+                  print('You are WRONG!');
+                }
+                setState(() {
+                  currentQ++;
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreList,
+        ),
       ],
     );
   }
